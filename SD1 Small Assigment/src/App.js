18e8bab07d29a400457188e34aa47feecb097e1d
ThatDudeAlex <pureawosome@gmail.com>
2019-03-16 20:12:18 -0400

    update

--- SD1 Small Assigment/src/App.js
+++ SD1 Small Assigment/src/App.js
@@ -3,74 +3,81 @@ import './App.css';
 
 class App extends Component {
 
- constructor(props) {
-  super(props);
-  this.state = {
-    source: props.source,
-    timestamp: props.timestamp
-  };
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      source: props.source,
+      timestamp: props.timestamp
+    };
+  }
 
+  playVideo() {
+    this.refs.vidRef.play();
+  }
+  pauseVideo() {
+    this.refs.vidRef.pause();
+  }
 
+  timestampVideo() {
+    console.log(this.refs.vidRef.currentTime);
+  }
 
-playVideo() {
-  this.refs.vidRef.play();
-}
-pauseVideo() {
-  this.refs.vidRef.pause();
-}
+  forwardVideo() {
+    this.refs.vidRef.currentTime += 0.5;
+    this.timestampVideo();
+  }
 
-timestampVideo() {
-  console.log(this.refs.vidRef.currentTime);
-}
+  rewindVideo() {
+    this.refs.vidRef.currentTime -= 0.5;
+    this.timestampVideo();
+  }
 
-forwardVideo() {
-  this.refs.vidRef.currentTime += 0.5;
-  this.timestampVideo();
-}
+  restartVideo() {
+    this.refs.vidRef.currentTime = 0.0;
+    this.timestampVideo();
+  }
 
-rewindVideo() {
-  this.refs.vidRef.currentTime -= 0.5;
-  this.timestampVideo();
-}
+  getVideoLenght() {
+    console.log(this.refs.vidRef.duration);
+  }
 
-restartVideo() {
-  this.refs.vidRef.currentTime = 0.0;
-  this.timestampVideo();
-}
+  arrowKeys = keypress => {
+    this.arrowKeyHandler(keypress.key);
+  }
 
-handleKey = keypress => {
-  console.log(keypress.keyCode);
-  this.arrowKeyHandler(keypress.keyCode);
-}
+  arrowKeyHandler = keycode => {
+    console.log(keycode);
+    if (keycode === 'ArrowLeft')
+      this.rewindVideo();
+    else if (keycode === 'ArrowRight')
+      this.forwardVideo();
+  }
 
-arrowKeyHandler = keycode => {
-  if(keycode === 37)
-     this.rewindVideo();
-  else if(keycode === 39)
-    this.forwardVideo();
-}
-
-
-  render() {
-	return (
-  	<div className="App"  tabIndex="0" onKeyDown={this.handleKey}>
-
-    	<video ref="vidRef" width="560" height="315">
-      	<source  id="testVideo" src="http://techslides.com/demos/sample-videos/small.ogv" type="video/ogg" />
-    	</video>
-
-      <div>
-        <button onClick={this.playVideo.bind(this)}>PLAY</button>
-        <button onClick={this.pauseVideo.bind(this)}>PAUSE</button>
-        <button onClick={this.forwardVideo.bind(this)}>FORWARD</button>
-        <button onClick={this.rewindVideo.bind(this)}>REWIND</button>
-        <button onClick={this.restartVideo.bind(this)}>RESTART</button>
-        <button onClick={this.timestampVideo.bind(this)}>TIMESTAMP</button>
-      </div>  
-  	</div>
-  );
   
+  render() {
+    return (
+      <div className="App" tabIndex="0" onKeyDown={this.arrowKeys}>
+
+        <video ref="vidRef" width="560" height="315">
+          <source id="testVideo" src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.ogv" type="video/ogg" />
+        </video>
+
+        <div>
+          <button onClick={this.playVideo.bind(this)}>PLAY</button>
+          <button onClick={this.pauseVideo.bind(this)}>PAUSE</button>
+          <button onClick={this.forwardVideo.bind(this)}>FORWARD</button>
+          <button onClick={this.rewindVideo.bind(this)}>REWIND</button>
+          <button onClick={this.restartVideo.bind(this)}>RESTART</button>
+        </div>
+
+        <div>
+          <button onClick={this.timestampVideo.bind(this)}>TIMESTAMP</button>
+          <button onClick={this.getVideoLenght.bind(this)}>VIDEO LENGHT</button>
+
+        </div>
+      </div>
+    );
+
   }
 
 }
